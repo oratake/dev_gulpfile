754f3fd6932d7e8ebe4e3f49d1489b7913fada4d
oratake <smith.shimomura@gmail.com>
2018-12-21 11:48:44 +0900

    [add] plumber notify prefixer WIP
    prefixerまだ動いてない。
    notifyはwinのWSL上でうまいこといかない。
    ↓powershell.exe叩く力技。参考になるかも
    https://cobodo.hateblo.jp/entry/2018/03/31/034144

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,16 @@
 // plugins
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const browserSync = require('browser-sync');
+const gulp         = require('gulp');
+const sass         = require('gulp-sass');
+const plumber      = require('gulp-plumber');
+const notify       = require('gulp-notify');
+const browserSync  = require('browser-sync');
+const autoprefixer = require('gulp-autoprefixer');
 
 const paths = {
 	'scss': './src/sass/',
-	'css': './docs/css/',
+	'css' : './docs/css/',
 	'html': './docs/',
-	'js': './docs/scripts/'
+	'js'  : './docs/scripts/'
 }
 
 // sass options
@@ -15,19 +18,25 @@ const sassOptions = {
 	outputStyle: 'expanded'
 }
 
+// browserSync load
+const reload = (done) => {
+	browserSync.reload();
+	done();
+};
+
+
 // scss tasks
 gulp.task('scss', function() {
 	return gulp.src(paths.scss + '**/*.scss')
+		.pipe(plumber({
+			errorHandler: notify.onError("Error: <%= error.message %>")
+		}))
 		.pipe(sass(sassOptions))
 		.on('error', sass.logError)
 		.pipe(gulp.dest(paths.css));
 });
 
 
-const reload = (done) => {
-  browserSync.reload();
-  done();
-};
 //Browser Sync
 gulp.task('browser-sync', () => {
 	browserSync({
@@ -35,13 +44,13 @@ gulp.task('browser-sync', () => {
 			baseDir: paths.html
 		}
 	});
-	gulp.watch(paths.js + '**/*.js', gulp.task('reload'));
+	gulp.watch(paths.js + '**/*.js', reload);
 	gulp.watch(paths.html + '**/*.html', reload);
-	gulp.watch(paths.css + '**/*.css', gulp.task('reload'));
+	gulp.watch(paths.css + '**/*.css', reload);
 });
-gulp.task('reload', () => {
-	browserSync.reload();
-});
+// gulp.task('reload', () => {
+// 	browserSync.reload();
+// });
 
 // watch
 gulp.task('watch', function() {
